3da53fe8962d7c559d9772c63ebb56fc28f935a1
Lenanguyen04 <lenanguyenlb04@gmail.com>
2025-11-12 18:40:03 -0800

    Initial adblocker

---
+++ hiding.js
@@ -0,0 +1,7 @@
+const elements = document.getElementsByClass("container__ads stack__ads")
+const elements1 = document.getElementsByClass("ad-slot__feedback ad-feedback-link-container")
+const elements2 = document.getElementsByClass("adContainer")
+
+elements.remove()
+elements1.remove()
+elements2.remove()
